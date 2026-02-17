ffe431f77be4183fab82f2e6c8497f9811316b66
Cody Cintron <crc24c@fsu.edu>
2026-02-17 09:53:31 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Prep Yield</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  );
+}
+
+export default App;
